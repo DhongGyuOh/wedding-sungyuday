1b86a1aa6d960744f1f59c5253687fb9dd003bcb
DhongGyuOh <mroh1226@naver.com>
2025-04-29 00:31:06 +0900

    이미지 변경

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"0aac603420054d9b6ecf6866d825790b4f875a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2062749575"
+    serviceWorkerVersion: "1953649623"
   }
 });
